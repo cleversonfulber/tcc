41e2d2831d6979da7f51a025dd26efadd381a544
Cleverson Fulber <cleversonfulber@alunos.utfpr.edu.br>
2019-09-09 22:44:28 -0300

    carrossel

--- src/main/resources/static/js/carrossel.js
+++ src/main/resources/static/js/carrossel.js
@@ -3,28 +3,28 @@ $(document).ready(function(){
 var speed = 5000;
 var run = setInterval('rotate()', speed);
 
-var item_width = $('itens li').outerWidth();
+var item_width = $('#itens li').outerWidth();
 var left_value = item_width * (-1);
 
-$('#itens li: first').before($('itens li:last'));
+$('#itens li:first').before($('#itens li:last'));
 
-$('#itens ul').css({'left': left_value});
+$('#itens ul').css({'#left': left_value});
 
 $("prev").click(function(){
-    var left_intend = parseInt($('#itens ul').css('left')) + item_width;
-    $('#itens ul').animate({'left':left_intend}, 200, function(){
-        $('#itens li: first').before($('itens li:last'));
-        $('#itens ul').css({'left': left_value});
+    var left_intend = parseInt($('#itens ul').css('#left')) + item_width;
+    $('#itens ul').animate({'#left':left_intend}, 200, function(){
+        $('#itens li:first').before($('#itens li:last'));
+        $('#itens ul').css({'#left': left_value});
     });
     clearInterval(run);
     run = setInterval('rotate()', speed);
 });
 
 $("next").click(function(){
-    var left_intend = parseInt($('#itens ul').css('left')) - item_width;
-    $('#itens ul').animate({'left':left_intend}, 200, function(){
-        $('#itens li: last').after($('itens li:first'));
-        $('#itens ul').css({'left': left_value});
+    var left_intend = parseInt($('#itens ul').css('#left')) - item_width;
+    $('#itens ul').animate({'#left':left_intend}, 200, function(){
+        $('#itens li:last').after($('#itens li:first'));
+        $('#itens ul').css({'#left': left_value});
     });
 
     clearInterval(run);
